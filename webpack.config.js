eaf0671cf3582c2c983d37b83a68c07d8d18668f
Chihiro Yamamoto <c_yamamoto@corp.allabout.co.jp>
2020-01-23 19:30:20 +0900

    ビルドJob追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,12 @@
 module.exports = {
     // モード値を production に設定すると最適化された状態で、
     // development に設定するとソースマップ有効でJSファイルが出力される
-    mode: "production",
-  
-    module: {
-      rules: [
-        {
-          // 拡張子 .js の場合
-          test: /\.js$/,
-          use: [
-            {
-              // Babel を利用する
-              loader: "babel-loader",
-              // Babel のオプションを指定する
-              options: {
-                presets: [
-                  // プリセットを指定することで、ES2019 を ES5 に変換
-                  "@babel/preset-env",
-                  // React の JSX を解釈
-                  "@babel/react"
-                ]
-              }
-            }
-          ]
-        }
-      ]
-    }
-  };
+    mode: "development",
+    entry: [ 
+        __dirname + '/src/app.jsx' 
+    ], 
+    output: { 
+        path: __dirname + '/dist', 
+        filename: 'bundle.js' 
+    }, 
+};
