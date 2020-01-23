76e37f54f0e69b571dbfc5ed7e6b042b597b5e80
Chihiro Yamamoto <c_yamamoto@corp.allabout.co.jp>
2020-01-23 20:10:05 +0900

    サンプルアプリ追加

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {HelloWorld} from './sub.jsx';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <HelloWorld name="橘真琴" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#app'));
