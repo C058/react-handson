bdeae871035cf7fa79d60bfce5a8a3c640bfd623
Chihiro Yamamoto <c_yamamoto@corp.allabout.co.jp>
2020-01-23 20:57:47 +0900

    TODOLISTの雛形作った

--- src/app.jsx
+++ src/app.jsx
@@ -6,8 +6,24 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <HelloWorld name="橘真琴" />
-        <div>あああああああああ</div>
+        <form class="input-form">
+          <input type="text"></input>
+          <input type="submit"></input>
+        </form>
+        <ul class="list">
+          <li class="list-item">
+            <div class="task-title">タスクタイトル1</div>
+            <div class="task-description">タスクの説明1</div>
+          </li>
+          <li class="list-item">
+            <div class="task-title">タスクタイトル2</div>
+            <div class="task-description">タスクの説明2</div>
+          </li>
+          <li class="list-item">
+            <div class="task-title">タスクタイトル3</div>
+            <div class="task-description">タスクの説明3</div>
+          </li>
+        </ul>
       </div>
     );
   }
