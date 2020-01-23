c536c8296132e11126a2de9b573698eeb7c34541
Chihiro Yamamoto <c_yamamoto@corp.allabout.co.jp>
2020-01-23 21:07:53 +0900

    値を受け取って表示できるようにした

--- src/todoList/list.js
+++ src/todoList/list.js
@@ -5,9 +5,9 @@ export class List extends React.Component {
   render() {
     return (
         <ul class="list">
-            <Task/>
-            <Task/>
-            <Task/>
+            <Task title="八百屋に行く" description="じゃがいも、にんじん、玉ねぎを買う"/>
+            <Task title="肉屋に行く" description="鶏肉を買う"/>
+            <Task title="米屋に行く" description="新之助を買う"/>
         </ul>
     );
   }
--- src/todoList/task.js
+++ src/todoList/task.js
@@ -4,8 +4,8 @@ export class Task extends React.Component {
   render() {
     return (
         <li class="list-item">
-        <div class="task-title">タスクタイトル2</div>
-        <div class="task-description">タスクの説明2</div>
+        <div class="task-title">{this.props.title}</div>
+        <div class="task-description">{this.props.description}</div>
         </li>
     );
   }
